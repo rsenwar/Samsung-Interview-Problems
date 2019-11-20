e4795ed612db3091129500caf7b4b4952f8cc606
Rakesh <rsenwar448@gmail.com>
2019-11-21 02:49:57 +0530

    Create 2.cpp

---
+++ 6. Burst Balloons/2.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+using namespace std; 
+  
+// recursive function to generate scores 
+int getmaxscore(int arr[], int l, int r, int n) 
+{ 
+    int mscore = 0; 
+    for (int i = l + 1; i < r; i++) { 
+  
+        // to permute through all cases 
+        int cs = getmaxscore(arr, l, i, n) + getmaxscore(arr, i, r, n); 
+        if (l == 0 && r == n) 
+            cs = cs + arr[i]; 
+        else
+            cs = cs + (arr[l] * arr[r]); 
+  
+        if (cs > mscore) 
+            mscore = cs; 
+    } 
+    return mscore; 
+} 
+  
+int main() // driver function 
+{ 
+    int n;
+    cin>>n;
+    int arr[15];  
+      arr[0]=1 && arr[n+1]=1 
+      cin>>n; 
+      for(int i=1;i<=n;i++) 
+         cin>>arr[i]; */
+  
+    cout << getmaxscore(arr, 0, n + 1, n + 1) << "\n"; 
+  
+    return 0; 
+} 
